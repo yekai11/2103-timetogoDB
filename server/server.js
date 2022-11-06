7aaf743d5f2b3d754b97c528a0aa015180209da7
nacuy <2100888@sit.singaporetech.edu.sg>
2022-11-06 16:45:32 +0800

    Basic server setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Parse incoming cookies and request bodies
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable all CORS requests
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('ICT2103');
+});
+
+// Start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}.`);
+});
