b47b81a612f061c4b59da344d4ca9043c298b3f3
meloonboi <jkllim361@gmail.com>
2022-11-17 22:37:55 +0800

    renamed database to small letter h for houseDB

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     password: 'root',
-    database: 'HouseDB',
+    database: 'houseDB',
     host: 'localhost',
     port: 5432
 });
