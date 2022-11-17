48c73713b96636c7664d07bdbbc88a1632db6ebf
meloonboi <jkllim361@gmail.com>
2022-11-17 22:34:58 +0800

    Fixed server error

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    password: 'root',
-    database: 'HouseDB',
+    user: 'user1',
+    password: '0088',
+    database: 'houseDB',
     host: 'localhost',
     port: 5432
 });
