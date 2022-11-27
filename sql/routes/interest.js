b8aecbf074aac06e07a3edf679ad7dedee4305a5
meloonboi <jkllim361@gmail.com>
2022-11-27 15:14:03 +0800

    Added more interest endpoints

--- sql/routes/interest.js
+++ sql/routes/interest.js
@@ -20,16 +20,51 @@ router.post("/addInterest", async (req, res) => {
   }
 });
 
+/* Endpoint to delete interest */
 router.post("/deleteInterest", async (req, res) => {
   try {
     const { interest_id } = req.body; // setting objects for easy reference
-    console.log(area); // for debugging
+    console.log(interest_id); // for debugging
 
     const deleteInterestQuery = await pool.query(
       `DELETE FROM Interest WHERE interest_id = ${interest_id};`
     );
 
     res.sendStatus(204);
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/* Endpoint to get all listings where 1 account is interested in */
+router.get("/accountInterest", async (req, res) => {
+  try {
+    const { account_id } = req.body; // setting objects for easy reference
+    console.log(account_id); // for debugging
+
+    const accountInterestQuery = await pool.query(
+      `SELECT listing_id FROM Interest WHERE account_id = ${account_id};`
+    );
+
+    res.json(accountInterestQuery.rows);
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/* Endpoint to get number of accounts interested in flat */
+router.get("/numAccountsInterest", async (req, res) => {
+  try {
+    const { listing_id } = req.body; // setting objects for easy reference
+    console.log(listing_id); // for debugging
+
+    const numInterestQuery = await pool.query(
+      `SELECT COUNT(interest_id) FROM Interest WHERE listing_id = ${listing_id};`
+    );
+
+    res.json(numInterestQuery.rows);
     return;
   } catch (err) {
     console.log(err);
