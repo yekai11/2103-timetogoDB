0b3027279e433ba838213972a7bb89f18b46fe20
2100888 <2100888@sit.singaporetech.edu.sg>
2022-11-30 16:15:39 +0800

    NoSQL: Fixed some endpoints

--- nosql/routes/interest.js
+++ nosql/routes/interest.js
@@ -10,12 +10,12 @@ router.post("/addInterest", async (req, res) => {
     const { account_id, listing_id } = req.body; // setting objects for easy reference
     console.log(account_id); // for debugging
 
-        // Add interest to interested array field
-        db.collection("account").updateOne({ account_id: parseInt(account_id) }, { $push: { interested: { listing_id: parseInt(listing_id) } } }, (err, result) => {
-            assert.equal(null, err);
-            res.sendStatus(201);
-            database.close();
-        });
+    // Add interest to interested array field
+    db.collection("account").updateOne({ account_id: parseInt(account_id) }, { $push: { interested: { listing_id: parseInt(listing_id) } } }, (err, result) => {
+      assert.equal(null, err);
+      res.sendStatus(201);
+      database.close();
+    });
 
     return;
   } catch (err) {
@@ -53,20 +53,21 @@ router.get("/accountInterest/:account_id", async (req, res) => {
     const { account_id } = req.params; // setting objects for easy reference
     console.log(account_id); // for debugging
 
-        let listings = [];
-        const account = await db.collection("account").findOne({ account_id: parseInt(account_id) });
-        if (account.interested !== null) {
-            account.interested.forEach(listing => {
-                const interest = db.collection("listing").findOne({ listing_id: listing.listing_id });
-                listings.push(interest);
-            });
-        }
+    let listings = [];
+    const account = await db.collection("account").findOne({ account_id: parseInt(account_id) });
+    if (account.interested !== null) {
+      account.interested.forEach(listing => {
+        const interest = db.collection("listing").findOne({ listing_id: listing.listing_id });
+        listings.push(interest);
+      }, () => {
         res.json(listings);
         database.close();
-        return;
-    } catch (err) {
-        console.log(err);
+      });
     }
+    return;
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
