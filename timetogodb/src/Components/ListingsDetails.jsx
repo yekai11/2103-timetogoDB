d2fa1f804f540be54cb318038603b0e7e3b31f05
Danial <danialhbma@gmail.com>
2022-11-30 18:57:34 +0800

    Update ListingsDetails.jsx

--- timetogodb/src/Components/ListingsDetails.jsx
+++ timetogodb/src/Components/ListingsDetails.jsx
@@ -2,12 +2,13 @@ import { Typography, Box, Button, Slide, Grid } from "@mui/material";
 import React, { useEffect, useMemo } from "react";
 // pictures
 import listingHeader from "../assets/listingHeader.jpg";
-import rentListIcon from "../assets/rentListIcon.jpg";
 // various icons
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import HeightIcon from "@mui/icons-material/Height";
 import ApartmentIcon from "@mui/icons-material/Apartment";
 import LayersIcon from "@mui/icons-material/Layers";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 // these are bootstrap card configurations
 import { Card } from "react-bootstrap";
 import { ListGroup } from "react-bootstrap";
@@ -29,7 +30,7 @@ export default function DetailsSection() {
   const account_id = window.localStorage.getItem("accountID");
   console.log(account_id);
   const handleClickOpen = () => {
-    fetch(`http://localhost:5000/addInterest`, {
+    fetch(`http://localhost:5000/interest/addInterest`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -39,11 +40,8 @@ export default function DetailsSection() {
         account_id: account_id,
         listing_id: listing_id,
       }),
-    }).then((result) => {
-
-    });
+    }).then((result) => {});
   };
-
 
   const [accountID, setAccountID] = React.useState(
     window.localStorage.getItem("accountID")
@@ -51,17 +49,19 @@ export default function DetailsSection() {
 
   /* useEffect to get all listings and display */
   useEffect(() => {
-    fetch(`http://localhost:5000/listing/getListing/${listing_id}`).then((result) => {
-      result.json().then((response) => {
-        /*this response is where the json is stored at.
+    fetch(`http://localhost:5000/listing/getListing/${listing_id}`).then(
+      (result) => {
+        result.json().then((response) => {
+          /*this response is where the json is stored at.
           its a huge array of objects like CardInfo example below
           Need to setup DB to know what is sent back
           */
-        console.log(response);
-        setCardInfo(response);
-        setCurrentPage(1);
-      });
-    });
+          console.log(response);
+          setCardInfo(response);
+          setCurrentPage(1);
+        });
+      }
+    );
   }, []);
 
   return (
@@ -80,23 +80,69 @@ export default function DetailsSection() {
           alt="banner"
           style={{
             zIndex: -1,
-            height: "500px",
+            height: "80%",
             width: "100%",
             position: "absolute",
             objectFit: "cover",
+            opacity: 0.8,
           }}
           src={listingHeader}
         ></img>
         <Typography
           variant="h3"
+          color="common.white"
           sx={{
             fontWeight: 600,
-            paddingLeft: 5,
-            textShadow: "1px 1px 2px white",
-            paddingTop: "300px",
+            paddingLeft: 10,
+            textShadow: "2px 2px 2px black",
+            paddingTop: "200px",
           }}
         >
-          {card.num_of_rooms}
+          {card.area}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          height: "10%",
+          alignItems: "center",
+          mt: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          color="common.white"
+          sx={{
+            fontWeight: 600,
+            paddingLeft: 10,
+            textShadow: "2px 2px 2px black",
+          }}
+        >
+          {card.block}, {card.street}, {card.postal_code},
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          height: "10%",
+          alignItems: "center",
+          mt: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          color="common.white"
+          sx={{
+            fontWeight: 600,
+            paddingLeft: 10,
+            textShadow: "2px 2px 2px black",
+          }}
+        >
+          ${card.price}
         </Typography>
       </Box>
       <br></br>
@@ -105,29 +151,6 @@ export default function DetailsSection() {
         <Card style={{ width: "90%" }} className="ListCard">
           {/*key={index}*/}
           <Card.Body>
-            <Card.Title className="align-self-left">
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <ListGroup horizontal className="flex-fill border-0">
-                  <ListGroup.Item className="flex-fill border-0">
-                    <ApartmentIcon color="primary" fontSize="large" />
-                    {card.num_of_rooms}
-                  </ListGroup.Item>
-                  <ListGroup.Item className="flex-fill border-0">
-                    <LayersIcon color="primary" fontSize="large" />
-                    {card.floor_area_sqm} sqm
-                  </ListGroup.Item>
-                </ListGroup>
-              </Box>
-            </Card.Title>
-          </Card.Body>
-
-          <Card.Body>
             <Box
               sx={{
                 display: "flex",
@@ -137,12 +160,12 @@ export default function DetailsSection() {
             >
               <ListGroup horizontal className="flex-fill border-0">
                 <ListGroup.Item className="flex-fill border-0">
-                  <ApartmentIcon color="primary" fontSize="large" />
-                  {card.num_of_rooms}
+                  <FormatListBulletedIcon color="primary" fontSize="large" />
+                  Listing Type: {card.listing_type}
                 </ListGroup.Item>
                 <ListGroup.Item className="flex-fill border-0">
-                  <LayersIcon color="primary" fontSize="large" />
-                  {card.floor_area_sqm} sqm
+                  <HeightIcon color="primary" fontSize="large" />
+                  Storey Range: {card.storey_range}
                 </ListGroup.Item>
               </ListGroup>
             </Box>
@@ -159,11 +182,11 @@ export default function DetailsSection() {
               <ListGroup horizontal className="flex-fill border-0">
                 <ListGroup.Item className="flex-fill border-0">
                   <ApartmentIcon color="primary" fontSize="large" />
-                  {card.num_of_rooms}
+                  Flat Type: {card.num_of_rooms}
                 </ListGroup.Item>
                 <ListGroup.Item className="flex-fill border-0">
                   <LayersIcon color="primary" fontSize="large" />
-                  {card.floor_area_sqm} sqm
+                  Floor Area Size: {card.floor_area_sqm} sqm
                 </ListGroup.Item>
               </ListGroup>
             </Box>
@@ -179,21 +202,16 @@ export default function DetailsSection() {
             >
               <ListGroup horizontal className="flex-fill border-0">
                 <ListGroup.Item className="flex-fill border-0">
-                  <ApartmentIcon color="primary" fontSize="large" />
-                  {card.num_of_rooms}
-                </ListGroup.Item>
-                <ListGroup.Item className="flex-fill border-0">
-                  <LayersIcon color="primary" fontSize="large" />
-                  {card.floor_area_sqm} sqm
+                  <DateRangeIcon color="primary" fontSize="large" />
+                  Datetime of Listing: {card.date_of_listing}
                 </ListGroup.Item>
               </ListGroup>
             </Box>
           </Card.Body>
 
           <Card.Body className="ListButton">
-            <Button onclick={handleClickOpen} variant="contained" size="large">
-              {" "}
-              <FavoriteIcon color="primary" fontSize="large" />
+            <Button onClick={handleClickOpen} variant="contained" size="large">
+              <FavoriteIcon color="secondary" fontSize="large"/>
               Add Interest
             </Button>
           </Card.Body>
